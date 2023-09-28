06eb3dab5a7582db46869c935aa9d40164f161aa
dasithakumbukage <dasitha@sonicdevs.com>
2023-09-28 15:51:04 +0530

    coinpayments done

--- src/coin-payment/coin-payment.controller.ts
+++ src/coin-payment/coin-payment.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseInterceptors,
+  Query,
+  Logger,
+  Get,
+  Headers,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CoinPaymentService } from './coin-payment.service';
 
 @Controller('coin-payment')
-export class CoinPaymentController {}
+export class CoinPaymentController {
+  constructor(private readonly coinPaymentService: CoinPaymentService) {}
+  //create coin-payment txn
+  @Post('create')
+  async createCoinPayment(@Body() body: any): Promise<any> {
+    return await this.coinPaymentService.createCoinPayment(body);
+  }
+
+  //listen to the IPN url
+  @Post('coin-payment-webhook')
+  @UseInterceptors(FileInterceptor('file'))
+  async listenToWebhook(
+    @Body() callBackData: any,
+    @Headers('hmac') hmac: any,
+  ) {
+    return await this.coinPaymentService.handleCallBackdetails(
+      callBackData,
+      hmac,
+    );
+  }
+}
